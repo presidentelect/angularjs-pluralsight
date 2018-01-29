9793fb5b845e1d8a3030fa0f7ebdd9a94716c5ae
Michael Hansen <m.hansen@pobox.com>
2018-01-29 17:45:51 -0500

    updates

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
-// Code goes here
 
-var MainController = function($scope){
-    $scope.message = "Hello, Angular!";
-  }
-  
+(function () {
+
+  var app = angular.module("githubViewer", []);
+
+  var MainController = function ($scope, $http, $interval, $log) {
+
+    var onUserComplete = function (response) {
+      $scope.user = response.data;
+      $http.get($scope.user.repos_url)
+        .then(onRepos, onError);
+    }
+
+    var onRepos = function (response) {
+      $scope.repos = response.data;
+    };
+
+    var onError = function (reason) {
+      $scope.error = "Could not fetch the data"
+    }
+
+    var decrementCountdown = function () {
+      $scope.countdown -= 1;
+      if ($scope.countdown < 1) {
+        $scope.search($scope.username);
+      }
+    }
+
+    var startCountdown = function () {
+      $interval(decrementCountdown, 1000, $scope.countdown);
+    }
+
+    $scope.search = function (username) {
+      $log.info("Searching for " + username);
+      $http.get("https://api.github.com/users/" + username)
+        .then(onUserComplete, onError);
+    };
+
+    $scope.username = 'angular';
+    $scope.message = "GitHub Viewer";
+    $scope.repoSortOrder = "-stargazers_count";
+    $scope.countdown = 5;
+    startCountdown();
+  };
+
+  app.controller("MainController", ["$scope", "$http", "$interval", "$log", MainController]);
+
+}());
