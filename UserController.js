3e9a0ab4b75438ac4716d5a0dfe873fba5423e1c
Michael Hansen <m.hansen@pobox.com>
2018-01-31 18:20:41 -0500

    add routes and views

---
+++ UserController.js
@@ -0,0 +1,32 @@
+
+(function () {
+
+    var app = angular.module("githubViewer");
+
+    var UserController = function ($scope, github, $routeParams) {
+
+        var onUserComplete = function (data) {
+            $scope.user = data;
+            github.getRepos($scope.user).then(onRepos, onError);
+        }
+
+        var onRepos = function (response) {
+            $scope.repos = response.data;
+        };
+
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the data"
+        }
+
+ 
+
+
+        $scope.username = $routeParams.username;
+        $scope.repoSortOrder = "-stargazers_count";
+        github.getUser($scope.username).then(onUserComplete, onError);
+    };
+
+    app.controller("UserController",
+        ["$scope", "github", "$routeParams", UserController]);
+
+}());
